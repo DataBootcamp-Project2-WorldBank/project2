ea348660278d01ada46aa545ae844768c9654214
j0serobles <engjoserobles@gmail.com>
2021-04-18 13:10:42 -0400

    Added Users and Logons tables.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,6 +6,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    logons = db.relationship('Logon', backref='logged_user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+
+class Logon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip_address = db.Column(db.String(32))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Logon {}>'.format(self.body)
